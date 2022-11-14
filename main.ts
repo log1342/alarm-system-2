aea57c8a1cb9db981f8bc06277f267968b3787fc
log1342 <113910996+log1342@users.noreply.github.com>
2022-11-14 09:48:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 function Alarm () {
-    if (alarm == 1) {
+    if (alarm >= 1) {
         radio.sendString("Robber")
     }
 }
 input.onGesture(Gesture.ThreeG, function () {
     alarm += 1
     counter += 1
+    basic.showNumber(counter)
+    Alarm()
 })
-let alarm = 0
-alarm = 0
 let counter = 0
+let alarm = 0
+basic.showIcon(IconNames.Yes)
+alarm = 0
+counter = 0
 radio.setGroup(42)
 basic.forever(function () {
 	
